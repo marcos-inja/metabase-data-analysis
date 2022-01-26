b2d6eebc0e832c2b0574e30093dd588148f45403
marcos-inja <marcosvaas@gmail.com>
2022-01-25 23:53:04 -0300

    feat: adding page to 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="not_found">
+       404<span></span>Page not found.
+    </div>
+</template>
+
+<style scoped>
+    .not_found {
+        width: 100%;
+        height: 80vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    span{
+        height: 40px;
+        width: 1px;
+        background: black;
+        margin: 15px;
+    }
+</style>
