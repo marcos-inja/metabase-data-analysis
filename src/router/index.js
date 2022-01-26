f3455deb4574deac4c93df3fe55de48558016f8f
marcos-inja <marcosvaas@gmail.com>
2022-01-25 23:54:08 -0300

    feat: new routes for the about and 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Main from '../views/Analysis.vue'
+import About from "../views/About.vue";
+import NotFound from "../views/NotFound.vue";
+
 import VueRouter from "vue-router";
-// import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -10,7 +12,23 @@ const routes = [
     path: "/",
     name: "Home",
     component: Main,
-  },  
+  },
+  {
+    path: "/sobre",
+    name: "About",
+    component: About,
+  },
+  {
+    path: '/not-found', 
+    component: NotFound,
+    name: 'not-found'
+  },
+  {
+    
+    path: '/:catchAll(.*)',
+    redirect: { name: 'not-found' }
+    
+  }
 ];
 
 const router = new VueRouter({
