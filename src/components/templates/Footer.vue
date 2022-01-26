c18610cc4416c8a68ce1ac75de6269f05db4cf69
marcos-inja <marcosvaas@gmail.com>
2022-01-25 23:58:36 -0300

    feat: adding component footer

--- src/components/templates/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Ainda nada hehe</h1>
-    </div>
-</template>
---
+++ src/components/templates/FooterBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer class="bar_bottom  mt-5 p-4">
+        <span>Desenvolvido por <a href="https://github.com/marcos-inja">Marcos Vas</a> | 2022©</span>
+    </footer>
+</template>
+
+<style scoped>
+    .bar_bottom {
+        /* border-top: 2px black solid; */
+        /* height: 60px; */
+        -webkit-box-shadow: 0px -2px 7px -2px rgba(0,0,0,0.12); 
+box-shadow: 0px -2px 7px -2px rgba(0,0,0,0.12);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+
+    span {
+        margin: 0;
+        font-weight: 900;
+        font-size: 16px;
+    }
+</style>
+
