2316fb397726c4ecae8e9d052711a4b15dace185
marcos-inja <marcosvaas@gmail.com>
2022-01-28 23:28:02 -0300

    fix: from http to https

--- src/util/data.js
+++ src/util/data.js
@@ -1,7 +1,7 @@
 export default[
         {
             title: "Animes por quantidade de episódios", 
-            src: "http://m4rc0s.herokuapp.com/public/question/e1ebb124-3d66-4378-925e-2e9e8d67a011",
+            src: "https://m4rc0s.herokuapp.com/public/question/e1ebb124-3d66-4378-925e-2e9e8d67a011",
             desc: [
                 "O gráfico mostrado representa o numero de animes por quantidade de episodios, temos como base informações de 5 mil animes.",
                 "Podemos perceber que os animes com apenas um episódio dominam o gráfico. Uma teoria do por que disso, é por conta que muitas produtoras lançam um episódio de sonda, se for bem continua, se não partem para um próximo."
@@ -10,7 +10,7 @@ export default[
         },
         {
             title: "Quantidade de favoritos por duração do episódio",
-            src: "http://m4rc0s.herokuapp.com/public/question/81ee4cdc-d284-4a70-896d-95818ca7533c",
+            src: "https://m4rc0s.herokuapp.com/public/question/81ee4cdc-d284-4a70-896d-95818ca7533c",
             desc: [
                 "Existem animes com todo tipo de duração por episódio, alguns gigantes com episodios de 1 hora, outros bem curtos com episodios de 2 minutos, mas podemos notar que existe uma coisa em comum entre os animes que fazem os usuarios gostarem mais de alguns, e isso é a duração do episódio.",
                 "O grafico de pizza mostrado, representa a quantidade de favoritos que os animes com determinada duração receberam, podemos perceber que existe um preferencia por animes que tem uma duração de 20 a 25 minutos por episodio, e que os animes com essas durações também são muito famosos."
@@ -19,7 +19,7 @@ export default[
         },
         {
             title: "Quantidade de animes e quantidade de favoritos por categoria", 
-            src: "http://m4rc0s.herokuapp.com/public/question/2a3d947f-0adf-43ff-846e-9b619521f906",
+            src: "https://m4rc0s.herokuapp.com/public/question/2a3d947f-0adf-43ff-846e-9b619521f906",
             desc: [
                 "De acordo com o gráfico os animes que vem de uma mangá dominam tanto em numeros como na quantidade de favoritos, seguidos pelos animes originais que não tem uma origem.",
                 "Muitos animes que são consolidados em um mangá são adaptados para animes, como no caso do anime Naruto que é um dos mais famosos do site MyAnimeList e do mundo, por isso que tem essa quantidade de favoritos e essa quantidade de animes vindos dessa fonte."
@@ -28,7 +28,7 @@ export default[
         },
         {
             title: "Quantidade de episodios por classificação indicativa", 
-            src: "http://m4rc0s.herokuapp.com/public/question/eb8217a2-0d58-472e-8569-469cf86181f5",
+            src: "https://m4rc0s.herokuapp.com/public/question/eb8217a2-0d58-472e-8569-469cf86181f5",
             desc: [
                 "No grafico de pizza vemos que os animes que tem a classificação indicativa para o público adolescente tem uma quantidade maior de episódios e consequentemente no animes, seguidos pela classificação +17 que é dedicada para animes violentos destinados ao público jovem/adulto.",
                 "A maior quantidade de episódios da categoria PG-13 se deve ao fato de muitos animes do gênero shounen estão nela, essa é uma categoria com muitos animes longos, como Hunter x Hunter, Naruto, One Piece e Boku no Hero Academia."
@@ -37,7 +37,7 @@ export default[
         },
         {
             title: "Número de episódios por fonte", 
-            src: "http://m4rc0s.herokuapp.com/public/question/6a5f2419-ae59-41f0-b074-0fb57d968f85",
+            src: "https://m4rc0s.herokuapp.com/public/question/6a5f2419-ae59-41f0-b074-0fb57d968f85",
             desc: [
                 "Como mostrado em um grafico anterior os animes vindos de mangas dominam, por conta quantidade de animes e da quantidade de favoritos será estranho que ele não tivesse a maior quantidade de episódios.",
                 "Esse grafico reforça que os animes que tem origem do mangá são mais vistos pelas pessoas, mas ignorando os que vem de mangá, os originais também são bem numerosos, seguidos pelos vindos de light novel, que é numeroso pelo mesmo motivo dos vindos de mangá, que são adaptados de uma obra consolidada na fonte original."
